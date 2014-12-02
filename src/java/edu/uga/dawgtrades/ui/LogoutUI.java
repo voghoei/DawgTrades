4c19807177acb547af23f5925b1101fc9c9ba3a9
Daniel Guzman <daniel@guzman.io>
2014-12-02 14:06:03 -0500

    Add logout

---
+++ src/java/edu/uga/dawgtrades/ui/LogoutUI.java
@@ -0,0 +1,50 @@
+package edu.uga.dawgtrades.ui;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import edu.uga.dawgtrades.DTException;
+import edu.uga.dawgtrades.control.LoginControl;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author sahar
+ */
+public class LogoutUI extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        // Get current session
+        HttpSession session = request.getSession(true);
+        if(session.getAttribute("currentSessionUser") != null) {
+            session.removeAttribute("currentSessionUser");
+        }
+        // Already logged in, redirect.
+        response.sendRedirect("/");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        doGet(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Logout UI";
+    }// </editor-fold>
+
+}

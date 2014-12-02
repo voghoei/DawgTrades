969f471b6956303fea5bc8b71783b1fa063efab4
ANTOINETTEMARIAS <STEVEN3V@UGA.EDU>
2014-12-02 13:35:43 -0500

    changes

--- src/java/edu/uga/dawgtrades/ui/RegisterUI.java
+++ src/java/edu/uga/dawgtrades/ui/RegisterUI.java
@@ -9,7 +9,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletReponse;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class RegisterUI extends HttpServlet{

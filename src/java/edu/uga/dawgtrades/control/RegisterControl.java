e831ef4a0bc6ed7288681829fb99be390bd20226
ANTOINETTEMARIAS <STEVEN3V@UGA.EDU>
2014-12-02 12:36:27 -0500

    Added control for register

---
+++ src/java/edu/uga/dawgtrades/control/RegisterControl.java
@@ -0,0 +1,14 @@
+public class RegisterControl{
+
+	/** 
+		attemptToRegister
+		@return true if user was successfully added. false is an error occurred
+	*/
+	public boolean attemptToRegister(){
+	
+
+	}
+	
+	
+
+}

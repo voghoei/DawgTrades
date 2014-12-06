bdae62d00cf94cc85b5ec8b1a235610331d0b7e2
Daniel Guzman <daniel@guzman.io>
2014-12-06 14:45:10 -0500

    Fix commoncontrol imports

--- src/java/edu/uga/dawgtrades/control/CommonControl.java
+++ src/java/edu/uga/dawgtrades/control/CommonControl.java
@@ -14,6 +14,7 @@ import java.util.Iterator;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpSession;
+import java.util.*;
 
 public class CommonControl{
 	public static final Map<String, String> infoMessages;

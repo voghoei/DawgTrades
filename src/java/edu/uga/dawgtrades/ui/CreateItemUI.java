78662d45d0d713aac0efcbc98d33fcaa4f2588c7
ANTOINETTEMARIAS <STEVEN3V@UGA.EDU>
2014-12-07 11:00:07 -0500

    changes

---
+++ src/java/edu/uga/dawgtrades/ui/CreateItemUI.java
@@ -0,0 +1,38 @@
+package edu.uga.dawgtrades.ui;
+
+import edu.uga.dawgtrades.DTException;
+import edu.uga.dawgtrades.control.LoginControl;
+import edu.uga.dawgtrades.model.RegisteredUser;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class CreateAuctionUI extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+		HttpSession session = request.getSession(true);
+		LoginControl ctrl = new LoginControl();
+		if(!ctrl.checkIsLoggedIn(session)){
+			response.sendRedirect("/login");
+			request.setAttribute("loggedInUser","");
+			request.removeAttribute("loggedInUser");
+			return;
+		}else{
+			RegisteredUser currentUser = (RegisteredUser)session.getAttribute("currentSessionUser");
+			request.setAttribute("loggedInUser",currentUser);
+		}
+		request.getRequestDispatcher("/createItem.ftl").forward(request,response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+		
+			
+	}
+}

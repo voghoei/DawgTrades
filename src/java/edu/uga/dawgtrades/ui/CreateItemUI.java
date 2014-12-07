340a7a07100057b888c9222b6f5a95b7afd59ada
ANTOINETTEMARIAS <STEVEN3V@UGA.EDU>
2014-12-07 11:05:02 -0500

    changes

--- src/java/edu/uga/dawgtrades/ui/CreateItemUI.java
+++ src/java/edu/uga/dawgtrades/ui/CreateItemUI.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class CreateAuctionUI extends HttpServlet{
+public class CreateItemUI extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
 		HttpSession session = request.getSession(true);

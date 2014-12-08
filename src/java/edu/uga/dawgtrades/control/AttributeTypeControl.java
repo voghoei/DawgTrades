0789d28ba54fb32151d6002205e482e32022eeeb
Daniel Guzman <daniel@guzman.io>
2014-12-07 22:04:21 -0500

    Add attribute type handlers

---
+++ src/java/edu/uga/dawgtrades/control/AttributeTypeControl.java
@@ -0,0 +1,89 @@
+package edu.uga.dawgtrades.control;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import edu.uga.dawgtrades.DTException;
+import edu.uga.dawgtrades.control.LoginControl;
+import edu.uga.dawgtrades.model.*;
+import edu.uga.dawgtrades.model.impl.ObjectModelImpl;
+import edu.uga.dawgtrades.persistence.Persistence;
+import edu.uga.dawgtrades.persistence.impl.DbUtils;
+import edu.uga.dawgtrades.persistence.impl.PersistenceImpl;
+import edu.uga.dawgtrades.persistence.impl.ItemIterator;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.util.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author reanimus
+ */
+public class AttributeTypeControl {
+
+    private Connection conn = null;
+    private ObjectModel objectModel = null;
+    private Persistence persistence = null;
+    private boolean hasError = false;
+    private String error;
+
+    public boolean hasError() {
+        return this.hasError;
+    }
+
+    public String getError() {
+        String err = null;
+        if (this.hasError) {
+            err = this.error;
+            this.error = null;
+            this.hasError = false;
+        }
+        return err;
+    }
+
+    private void connect() throws DTException{
+            this.close();
+            this.conn = DbUtils.connect();
+            this.objectModel = new ObjectModelImpl();
+            this.persistence = new PersistenceImpl(conn,objectModel);
+            this.objectModel.setPersistence(persistence);
+        
+    }
+    private void close(){
+        try{
+            if(this.conn != null) {
+                this.conn.close();
+            }
+
+        }catch (Exception e){
+            System.err.println("Exception: "+e);
+        }
+    }
+    public boolean createAttributeType(long categoryID, String name, boolean isString) {
+        CategoryControl catCtrl = new CategoryControl();
+        if(catCtrl.categoryExists(categoryID)) {
+            try {
+                this.connect();
+                AttributeType type = this.objectModel.createAttributeType(catCtrl.getCategoryWithID(categoryID), name, isString);
+                this.objectModel.storeAttributeType(type);
+                return true;
+            }
+            catch(DTException e) {
+                this.hasError = true;
+                this.error = e.getMessage();
+                return false;
+            }
+            finally {
+                this.close();
+            }
+        }else{
+            return false;
+        }
+    }
+
+}

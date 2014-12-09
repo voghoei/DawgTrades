bea85de820c7f25d885487c263a82249c5980250
Daniel Guzman <daniel@guzman.io>
2014-12-09 04:01:24 -0500

    Formatting + logic issue in registeredusermanager

--- src/java/edu/uga/dawgtrades/model/impl/ObjectModelImpl.java
+++ src/java/edu/uga/dawgtrades/model/impl/ObjectModelImpl.java
@@ -269,7 +269,7 @@ public class ObjectModelImpl extends Persistent implements ObjectModel {
     }
 
     public RegisteredUser createRegisteredUser(String firstName, String lastName, String name, String password,  String email, String phone, boolean canText,boolean isAdmin, boolean isApproved) throws DTException {
-        RegisteredUser registeredUser = new RegisteredUserImpl(firstName, lastName, name, password, email, phone,  canText,isAdmin, isApproved);
+        RegisteredUser registeredUser = new RegisteredUserImpl(firstName, lastName, name, password, email, phone,  canText, isAdmin, isApproved);
         return registeredUser;
     }
 

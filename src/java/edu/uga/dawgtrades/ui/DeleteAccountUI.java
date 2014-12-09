8d740d50246ece0578f076649035afd68a741225
ANTOINETTEMARIAS <STEVEN3V@UGA.EDU>
2014-12-09 13:51:04 -0500

    changes

--- src/java/edu/uga/dawgtrades/ui/DeleteAccountUI.java
+++ src/java/edu/uga/dawgtrades/ui/DeleteAccountUI.java
@@ -47,7 +47,7 @@ public class DeleteAccountUI extends HttpServlet{
 	
 	 @Override
         protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
-
+		doGet(request,response);
 	}		
 	
 }
